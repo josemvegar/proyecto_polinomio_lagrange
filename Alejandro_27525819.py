f8e69103b9fce53a7cfcf0d6773f49ae30fab74b
Al3x28 <Alex25808@gmail.com>
2019-11-21 11:37:37 -0400

    metodo de Alejandro

--- Alejandro_27525819.py
+++ Alejandro_27525819.py
@@ -5,7 +5,8 @@ Proyecto Polinomio de Lagrange.
 Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 """
 
-def polinomio_lagrange(X, Y):
+
+def polinomio_lagrange(X,Y):
     """
     Implementa y retorna el Polinomio de Lagrange para las listas X e Y.
 
@@ -13,21 +14,36 @@ def polinomio_lagrange(X, Y):
     X: lista de valores de la variable independiente.
     Y: lista de valores de la variable dependiente.
     """
-
-     if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
-
+    if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
+   
     # Ordena el par (x, y) en forma ascendente por x.
-    pares = list(zip(X, Y))
-    pares.sort(key = lambda x: x[0])
-    X, Y  = zip(*pares)
+    z = list(zip(X,Y))
+    z.sort(key = lambda x: x[0])
+    print (z)
+    X, Y  = zip(*z)
 
-     def polinomio(x):
-        pass
-
-    return polinomio
+    def p(x):
+        r=[]  
+        result=0
+        n=len(X)
+        for i in range(n):
+            result1=1
+            r2=1
+            xk=X[i]
+            for j in range(n):  
+                if j!=i:
+                    xi=X[j]
+                    result1*=(x-xi)/float(xk-xi)
+            r.append(result1)
+        for k in range(n):
+            result = result+(Y[k]*r[k])
+        return result
+    return p
 
 
 if __name__ == '__main__':
-    # Pruebe aquí su Polinomio de Lagrange ...
-
-    pass
+    X=[-1.5,-0.5,1,-2,-4]
+    Y=[9,-2,5,33,0]
+    p=polinomio_lagrange(X,Y)
+    R=p(-4)
+    print (R)
