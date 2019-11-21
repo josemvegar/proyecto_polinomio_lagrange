116b1e7d2acdbf169d35930388ee54d0c6212480
josemvegar <josevega1999.16@gmail.com>
2019-11-21 19:26:43 -0400

    listo

--- Jose_27000723.py
+++ Jose_27000723.py
@@ -14,20 +14,33 @@ def polinomio_lagrange(X, Y):
     Y: lista de valores de la variable dependiente.
     """
 
-     if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
+    if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
 
     # Ordena el par (x, y) en forma ascendente por x.
     pares = list(zip(X, Y))
     pares.sort(key = lambda x: x[0])
     X, Y  = zip(*pares)
-
-     def polinomio(x):
-        pass
+    def polinomio(x):
+        poli=0
+        lal=0
+        for i in range(len(X)):
+            lalaux=1
+            for a in range(len(X)):
+                if (a!=i):
+                    lal=(x-X[a])/(X[i]-X[a])
+                    lalaux=lalaux*lal
+            poliaux=Y[i]*lalaux
+            poli=poli+poliaux
+        return poli
 
     return polinomio
 
 
 if __name__ == '__main__':
-    # Pruebe aquí su Polinomio de Lagrange ...
-
+    x=[2,1,-1]
+    y=[3,-2,1]
+    polres=polinomio_lagrange(x,y)
+    for i in range(len(x)):
+        print ("el valor para= ", x[i]," es igual a= ", polres(x[i]))
+    bye=input()
     pass
